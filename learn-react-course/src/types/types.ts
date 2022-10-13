dbe28d571b2c1478e6678c6b66bf9cebe60ce123
Artem Potlov <potlov.artem@gmail.com>
2022-10-13 11:47:37 +0300

    feat: add types

--- learn-react-course/src/types/types.ts
+++ learn-react-course/src/types/types.ts
@@ -6,3 +6,40 @@ export type TGood = {
   category: string;
   image: string;
 };
+
+export type THeaderLinks = {
+  name: string;
+  path: string;
+};
+
+export type TCity = {
+  value: string;
+  title: string;
+};
+
+export type TErrorVal = {
+  error?: boolean;
+  helperText?: string;
+};
+
+export type TFormErrorFields = {
+  userName: TErrorVal;
+  email: TErrorVal;
+  phoneNumber: TErrorVal;
+  birthday: TErrorVal;
+  city: TErrorVal;
+  gender: TErrorVal;
+  cashback: TErrorVal;
+  file: TErrorVal;
+};
+
+export type TFormStore = {
+  userName?: string;
+  email?: string;
+  phoneNumber?: string;
+  birthday?: string;
+  city?: string;
+  gender?: string;
+  cashback?: string[];
+  file?: string;
+};
