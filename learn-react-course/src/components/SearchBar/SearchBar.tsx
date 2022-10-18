b396b95a8d2146dc8143745597ea09ef6a838b3a
Artem Potlov <potlov.artem@gmail.com>
2022-10-18 18:19:41 +0300

    fix: fix component state, change styles, add two functions

--- learn-react-course/src/components/SearchBar/SearchBar.tsx
+++ learn-react-course/src/components/SearchBar/SearchBar.tsx
@@ -4,40 +4,41 @@ import { getSearchData, saveSearchData } from '../../utils/local-storage';
 
 type TProps = {
   dataTestId?: string;
+  setValue?: React.Dispatch<React.SetStateAction<string>>;
 };
 
-const SearchBar: React.FC<TProps> = ({ dataTestId }) => {
-  const [value, setValue] = useState('');
-  const valueRef = useRef(value);
-
-  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (!(event.target instanceof HTMLInputElement)) return;
-    setValue(event.target.value.trim());
-  };
+const SearchBar: React.FC<TProps> = ({ setValue, dataTestId }) => {
+  const [searchVal, setSearchVal] = useState(getSearchData() || '');
+  const searchValRef = useRef(searchVal);
 
   useEffect(() => {
-    const valFromLocalStorage = getSearchData();
-    if (!valFromLocalStorage) return;
-    setValue(valFromLocalStorage);
-  }, []);
-
-  useEffect(() => {
-    valueRef.current = value;
-  }, [value]);
+    searchValRef.current = searchVal;
+  }, [searchVal]);
 
   useEffect(() => {
     return () => {
-      saveSearchData(valueRef.current);
+      saveSearchData(searchValRef.current);
     };
   }, []);
+
+  const onHandleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!(event.target instanceof HTMLInputElement)) return;
+    setSearchVal(event.target.value.trim());
+  };
+
+  const onInputEnterPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (!(event.target instanceof HTMLInputElement && event.key === 'Enter') || !setValue) return;
+    setValue(event.target.value.trim());
+  };
 
   return (
     <SearchInput
       placeholder="Search by..."
       type="search"
       data-testid={dataTestId}
-      value={value}
-      onChange={onInputChange}
+      onKeyUp={onInputEnterPress}
+      value={searchVal}
+      onChange={onHandleChange}
     />
   );
 };
@@ -47,14 +48,20 @@ SearchBar.defaultProps = { dataTestId: 'search-bar' };
 export default SearchBar;
 
 const SearchInput = styled.input`
-  padding: 0 1rem;
-  border: 1px solid lightsteelblue;
-  height: 3rem;
-  border-radius: 0.25rem;
-  outline: 0;
+  height: 58px;
+  border: 1px solid #cfd3d5;
+  border-radius: 8px;
+  padding: 8px 16px;
+  font-size: 16px;
+  line-height: 1;
+  outline: none;
   transition: border 0.2s ease-in-out;
 
+  &::placeholder {
+    color: #abafb1;
+  }
+
   &:focus {
-    border: 1px solid steelblue;
+    border: 1px solid #5570f1;
   }
 `;
