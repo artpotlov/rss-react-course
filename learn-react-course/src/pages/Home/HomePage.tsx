cb509dc0876e884075421fe26844d0a871b020e7
Artem Potlov <potlov.artem@gmail.com>
2022-10-18 18:20:50 +0300

    feat: add states

--- learn-react-course/src/pages/Home/HomePage.tsx
+++ learn-react-course/src/pages/Home/HomePage.tsx
@@ -1,21 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SearchBar } from '../../components/SearchBar';
-import { goodsData } from '../../data';
 import { CardList } from '../../components/CardList';
 import styled from '@emotion/styled';
+import { TGood } from '../../types/types';
+import { getAllProducts, getLimitProducts } from '../../utils/api';
+import { Loader } from '../../components/Loader';
 
 type TProps = {
   dataTestId?: string;
 };
 
 const HomePage: React.FC<TProps> = ({ dataTestId }) => {
+  const [products, setProducts] = useState<TGood[]>([]);
+  const [searchVal, setSearchVal] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+        const products = await getAllProducts();
+        const filteringData = products.filter((product) =>
+          product.title.toLowerCase().includes(searchVal.toLowerCase())
+        );
+        setProducts(filteringData);
+        setLoading(false);
+      } catch (error) {
+        if (!(error instanceof ErrorEvent)) return;
+        console.error(error.message);
+      }
+    })();
+  }, [searchVal]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+        setProducts(await getLimitProducts());
+        setLoading(false);
+      } catch (error) {
+        if (!(error instanceof ErrorEvent)) return;
+        console.error(error.message);
+      }
+    })();
+  }, []);
+
   return (
     <section data-testid={dataTestId}>
       <HeaderContainer>
         <h1>Home Page</h1>
-        <SearchBar />
+        <SearchBar setValue={setSearchVal} />
       </HeaderContainer>
-      <CardList goods={goodsData} />
+      {loading && <Loader />}
+      {!loading && <CardList goods={products} />}
     </section>
   );
 };
